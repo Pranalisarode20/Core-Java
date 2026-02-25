c2223fb9b7a54ff48c908288b8dfd95b6329ba05
Pranalisarode20 <pranalisarode700@gmail.com>
2026-02-25 17:02:52 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,19 @@
+package com.tak;
+
+public class DataType {
+	public static void main(String []args) {
+		
+		// Byte Size
+		System.out.println("Byte ="+ Byte.BYTES + " Byte");
+		System.out.println("Short Byte ="+ Short.BYTES + " Byte");
+		System.out.println("Integer Byte ="+ Integer.BYTES + " Byte");
+		System.out.println("Long Byte ="+ Long.BYTES + " Byte");
+		System.out.println("Float Byte ="+ Float.BYTES + " Byte");
+		System.out.println("Double Byte ="+ Double.BYTES + " Byte");
+		System.out.println("Character Byte ="+ Character.BYTES + " Byte");
+		System.out.println("Boolean is not Define");
+		
+		
+	}
+
+}
