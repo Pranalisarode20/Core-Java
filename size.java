48960dc4da9bebc74ca7e4a0ab2f72980d6b7927
Pranalisarode20 <pranalisarode700@gmail.com>
2026-02-25 17:16:39 +0530

    Add files via upload

---
+++ size.java
@@ -0,0 +1,15 @@
+package com.tak;
+
+public class size {
+	public static void main(String[] args) {
+		System.out.println("Byte size is = " + Byte.SIZE + "size");
+		System.out.println("Short size is = " + Short.SIZE + "size");
+		System.out.println("Interger size is = " + Integer.SIZE + "size");
+		System.out.println("Long size is = " + Long.SIZE + "size");
+		System.out.println("Float size is = " + Float.SIZE + "size");
+		System.out.println("Double size is = " + Double.SIZE + "size");
+		System.out.println("Character size is = " + Character.SIZE + "size");
+		System.out.println("Boolean is not Define");
+	}
+
+}
